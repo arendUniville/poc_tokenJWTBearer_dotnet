cea7d09a761b2b849bc7bd8e19f60c7cbd196fca
Arend <matheus.arend@univille.br>
2023-10-08 13:58:15 -0300

    Criando uma privateKey de teste

---
+++ JwtBearerPOC/JwtBearerPOC/Config/Configuration.cs
@@ -0,0 +1,7 @@
+﻿namespace JwtBearerPOC.Config;
+
+public class Configuration
+{
+    //Este é apenas um exemplo de chave, a chave não deve ser em hipósete alguma ser registrada aqui no código ou compartilhada, também não deve ser uma chave simples sendo indicado ter mais que 35 caracteres.
+    public static string PrivateKey { get; set; } = "(*72kc)2+2&)@v2392(@42*9u42(*&#f1n$(*&24)(97H$6345U898s9";
+}
